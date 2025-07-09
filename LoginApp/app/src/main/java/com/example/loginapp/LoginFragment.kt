6b3097061286cdaaa10bbc28e5a961a9549aedaf
MohRasen <mohrasendriatalentar@mail.ugm.ac.id>
2025-07-09 13:29:52 +0700

    initial commit

---
+++ LoginApp/app/src/main/java/com/example/loginapp/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.loginapp.databinding.FragmentLoginBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.fragment.findNavController
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btn.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToDoneFragment()
+            findNavController().navigate(action)
+        }
+    }
+}
---
+++ LoginApp/app/src/main/java/com/example/loginapp/SplashFragment.kt
@@ -0,0 +1,33 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.loginapp.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+       binding = FragmentSplashBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btn.setOnClickListener {
+            val action = SplashFragmentDirections.actionSplashFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
+
+}
